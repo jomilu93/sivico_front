f7cec0def5f4357851eaa643b71f1735ea35244d
Luis <lutzcc1@gmail.com>
2023-08-05 13:25:10 -0600

    fix:

--- app.py
+++ app.py
@@ -7,8 +7,6 @@ from nltk.corpus import stopwords
 
 import requests
 
-nltk.download('stopwords')
-
 st.set_page_config(layout="wide")
 extra_stop_words = ['México', 'sistema', 'senadores', 'aumentar', 'acceso', 'manera', 'mejorar']
 stop_words = set(stopwords.words('spanish'))
